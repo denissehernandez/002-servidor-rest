9a902335b14f64c630f7f9e3a1fd76f33e8c2ebc
denisse.hernandez <hernandezdenisse504@gmail.com>
2020-12-04 17:27:15 -0600

    Realizada la ruta de usuarios con la funcion POST con BD

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Usuario = require('../models/usuario');
 const app = express();
 
 app.get('/usuario', function(req, res) {
@@ -9,25 +10,31 @@ app.get('/usuario', function(req, res) {
 });
 
 app.post('/usuario', function(req, res) {
-    let nombre = req.body.nombre;
     let body = req.body;
+    let usr = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password
+    });
+    
+    usr.save((err, usrDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ocurrio un error',
+                err
+            });
+        }
 
-    if(nombre === undefined){
-        res.status(400).json({
-            ok: 400,
-            mensaje: 'Favor de enviar el valor del nombre'
+        res.json({
+            ok: true,
+            msg: 'Usuario insertado con exito',
+            usrDB
         });
-    }else{
-
-         res.json({
-            ok: 200,
-            mensaje: 'Usuario insertado con exito'
-            
-        });
-    }
+    });
 });
 
-app.put('/usuario/:id/:nombre', function(req, res){
+app.put('/usuario/:id/:nombre', function(req, res) {
     let id = req.params.id;
     let nombre = req.params.nombre;
 
